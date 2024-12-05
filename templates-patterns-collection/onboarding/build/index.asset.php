ed815c8d94e296d68e6cc246f9ff5e649b5c3f31
Justin Gardner <justin@rhynorater.com>
2024-12-05 06:03:34 -0500

    1.2.16

--- templates-patterns-collection/onboarding/build/index.asset.php
+++ templates-patterns-collection/onboarding/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => 'e6a6594959f42356a8f4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => 'f1ec9672d24a6b701268');
